26970e5f07d199fbd6dd01e2309e6028840116ae
cesis <80533818+cesis@users.noreply.github.com>
2021-03-12 17:36:42 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1l2Q6iwDjHW-F18q_zRDITXabHmsEFf-155AOmf-wNPk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAcuI3TWMNiN00NKYWAtBE5BRoS31bOZ98';
